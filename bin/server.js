980ce8ccf4dd94add9e16c71ff7af573f8e8b5b6
Arthur Chaitter <arthur.chaitter47@gmail.com>
2020-04-04 16:40:03 -0300

    Separação do server + install nodemon

--- bin/server.js
+++ bin/server.js
@@ -1,29 +1,20 @@
-'use strict'
 //imports de pacotes
+const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('nodestr:server');
-const express = require('express');
 
-const app = express();
+
 //escolhendo a porta
 const port = normalizePort(process.env.PORT || '3333');
 app.set('port',port);
 
 //criando o web server
 const server = http.createServer(app);
-const router = express.Router();
 
-const route = router.get('/', (req,res,next) => {
-    res.status(200).send({
-        title: "Hello World!",
-        version: "1.0.0"
-    });
-});
 
 //escolhendo caminho e colocar o codigo para ouvir a porta
-app.use('/', route);
-
 server.listen(port); console.log('API Rodando na porta ' + port);
+server.on('listening', onListening);
 
 //função de normalização da porta
 
@@ -39,4 +30,13 @@ function normalizePort(val){
     }
 
     return false;
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+
 }
